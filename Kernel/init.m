b4a2ba74f37e66da903bf762113b3627b59432e2
Taliesin Beynon <github@taliesin.ai>
2021-02-19 16:34:10 +0200

    Only look for HyperArrays if it's not loaded.

--- Kernel/init.m
+++ Kernel/init.m
@@ -1,5 +1,9 @@
 
-
-Get["~/git/HyperArrays/Kernel/init.m"];
-(*Get["~/git/GraphTools/Kernel/init.m"];*)
-Get @ FileNameJoin[{FileNameDrop @ $InputFileName, "Walks.m"}];
+If[$VersionNumber < 12.2,
+  General::packagewlversion = "Package `` requires Wolfram Langauge version `` or later to operate.";
+  Message[General::packagewlversion, "GraphWalking`", 12.2]
+,
+  If[Contexts["HyperArrays`"] === {}, Get["~/git/HyperArrays/Kernel/init.m"]];
+  (*Get["~/git/GraphTools/Kernel/init.m"];*)
+  Get @ FileNameJoin[{FileNameDrop @ $InputFileName, "Walks.m"}];
+];
